14fb2180142171248afacbf99c9d1d711281b27e
JuanPlay27 <juandavid_mr27@hotmail.com>
2025-11-12 20:38:07 -0500

    crear una venta con credito o apartado, y una compra a credito(en desarrollo

--- backend/apartado_credito/urls.py
+++ backend/apartado_credito/urls.py
@@ -1,6 +1,7 @@
 from rest_framework import routers
 from django.urls import path, include
-from .views import ApartadoViewSet, CreditoViewSet, CuotaViewSet
+from rest_framework.routers import DefaultRouter
+from .views import ApartadoViewSet, CreditoViewSet, CuotaViewSet, DeudasPorClienteView
 
 router = routers.DefaultRouter()
 router.register(r"apartados", ApartadoViewSet)
@@ -9,4 +10,5 @@ router.register(r"cuotas", CuotaViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("deudas-por-cliente/<int:cliente_id>/", DeudasPorClienteView.as_view()),
 ]
