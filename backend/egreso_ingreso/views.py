7f10ee0e56035859625584ba897384a44f188f4f
Laura Caballero <lauraalejandracp@ufps.edu.co>
2025-11-26 21:07:39 -0500

    Update views.py

--- backend/egreso_ingreso/views.py
+++ backend/egreso_ingreso/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+# egreso_ingreso/views.py
+from rest_framework import viewsets
+from .models import Egreso, Ingreso
+from .serializers import EgresoSerializer, IngresoSerializer
 
-# Create your views here.
+class EgresoViewSet(viewsets.ModelViewSet):
+    queryset = Egreso.objects.all()
+    serializer_class = EgresoSerializer
+
+
+class IngresoViewSet(viewsets.ModelViewSet):
+    queryset = Ingreso.objects.all()
+    serializer_class = IngresoSerializer
